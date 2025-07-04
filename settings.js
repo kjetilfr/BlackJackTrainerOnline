26c52cb978db4def0f529f3b74ff1ce8b641c301
Kjetil F. <kjefriborg@gmail.com>
2025-07-04 17:53:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,31 +2,23 @@
 import { Game } from './game.js';
 import { UI } from './ui.js';
 
-const settingsForm = document.getElementById('settings-form');
-const settingsContainer = document.getElementById('settings-container');
-const gameContainer = document.getElementById('game-container');
+const form = document.getElementById('settings-form');
+const settingsDiv = document.getElementById('settings-container');
+const gameDiv = document.getElementById('game-container');
 
 export let currentSettings = null;
 
-settingsForm.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-  const f = new FormData(settingsForm);
+  const f = new FormData(form);
   currentSettings = {
     numberOfHands: parseInt(f.get('hands')),
     lateSurrender: f.get('lateSurrender') === 'on',
-    blackjackPayout: parseFloat(f.get('payout')),
-    insurance: f.get('insurance') === 'yes',
     dealerHitsSoft17: f.get('dealer17') === 'hit',
-    doubleAfterSplit: f.get('doubleAfterSplit') === 'yes',
-    maxSplitHands: parseInt(f.get('splitHands')),
-    resplitAces: f.get('resplitAces') === 'yes',
-    splitAcesDrawOneCardOnly: f.get('splitAcesDrawOne') === 'yes',
-    startingChips: parseInt(f.get('startingChips')),
-    initialBet: parseInt(f.get('initialBet'))
+    maxSplitHands: parseInt(f.get('splitHands'))
   };
-
-  settingsContainer.style.display = 'none';
-  gameContainer.style.display = 'block';
+  settingsDiv.style.display = 'none';
+  gameDiv.style.display = 'block';
   Game.start(currentSettings);
-  UI.init(Game, gameContainer);
+  UI.init(Game, gameDiv);
 });
